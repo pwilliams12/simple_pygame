56858a33d768d423416dd239e0cc12864339e5f3
Phil@REYNOLDS <endersshadow12@gmail.com>
2023-12-03 10:40:07 -0500

    Initial commit of Gale's game.

---
+++ test/senseiWu.py
@@ -0,0 +1,11 @@
+import numpy as np
+import teaKettle
+
+if __name__ == "__main__":
+    # print ("Creating game object...")
+    # game = Game()
+    # print ("Done. Starting run method")
+    # game.run()
+    print('I like tea')
+    myTea = teaKettle.TeaKettle()
+    myTea.poreTea()
---
+++ test/teaKettle.py
@@ -0,0 +1,7 @@
+class TeaKettle(object):
+    print('Tea Kettle class')
+
+    def poreTea(self):
+        print('Yummy tea')
+
+    temperature = 5400
